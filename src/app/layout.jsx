c7d2603cd488167937a1423463e1a09c53137df8
unknown <rregfhlty@gmail.com>
2023-08-27 12:02:52 -0700

    create the header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,16 @@
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     
-      <body>{children}</body>
+      <body>
+        {/* {header} */}
+        <Header />
+        {/* navbar */}
+        {/* searchbox */}
+        {children}
+      </body>
     </html>
-  )
+  );
 }
