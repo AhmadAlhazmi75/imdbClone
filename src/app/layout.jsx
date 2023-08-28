f6d2f6dbf74e0cecd49d6937a1b0257c2b4f488a
AhmadAlhazmi75 <rregfhlty@gmail.com>
2023-08-28 05:33:28 -0700

    create the navbar component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
           {/* {header} */}
           <Header />
           {/* navbar */}
+          <Navbar />
           {/* searchbox */}
           {children}
         </Providers>
