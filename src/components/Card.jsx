1ee99d45139027b5b2b1a6c1cd9b4d7a7ddf10f5
AhmadAlhazmi75 <rregfhlty@gmail.com>
2023-08-29 08:05:56 -0700

    fix responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ import { FiThumbsUp } from "react-icons/fi";
 
 export default function Card({ result }) {
   return (
-    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+    <div className="cursor-pointer p-3 hover:shadow-slate-400 shadow-md rounded-lg border border-slate-400 m-2 transition-shadow duration-200 group">
       <Link href={`/movie/${result.id}`}>
-        <h2 className="truncate mb-2 text-lg font-bold">
+        <h2 className="truncate mb-2 text-lg font-bold sm:text-xl">
           {result.title || result.name}
         </h2>
         <Image
@@ -15,7 +15,7 @@ export default function Card({ result }) {
           }`}
           width={500}
           height={300}
-          className="object-cover h-32 sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+          className="object-cover h-32 sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200 w-full"
           style={{
             maxWidth: "100%",
           }}
@@ -24,13 +24,13 @@ export default function Card({ result }) {
           alt="image is not available"
         ></Image>
         <div className="p-2">
-          <p className="line-clamp-2 text-md">{result.overview}</p>
-          <p className="flex items-center">
+          <p className="line-clamp-2 text-sm sm:text-md">{result.overview}</p>
+          <p className="flex items-center text-sm sm:text-md">
             {result.release_date || result.first_air_date}
-            <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}
+            <FiThumbsUp className="h-4 sm:h-5 mr-1 ml-3" /> {result.vote_count}
           </p>
         </div>
       </Link>
     </div>
-  );
+);
 }
