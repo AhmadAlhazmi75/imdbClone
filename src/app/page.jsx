eb0cbf77cc10dc65a1b0a47c19e470f5e1d0d048
AhmadAlhazmi75 <rregfhlty@gmail.com>
2023-09-04 12:05:24 -0700

    fix a bug

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,9 @@ export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
     `https://api.themoviedb.org/3/${
-      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+      genre === "fetchTopRated"
+        ? "movie/top_rated"
+        : "trending/all/week?language=en-US"
     }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
